c6b2f15618ce354fad2d54061efe8342621f9a77
admin <admin@iMac3-2.local>
2017-10-26 10:46:51 +0200

    Final quiz

--- index.php
+++ index.php
@@ -1,27 +1,297 @@
 <?php
-  /*
-  require(); //erreur en chargeant un fichier inexistant
-  require_once(); //insère seulement si cela n'a pas déjà été fait
+session_start();
+?>
 
-  include(); //pas d'erreur en chargeant un fichier inexistant
-  include_once(); //insère seulement si cela n'a pas déjà été fait
-  */
-  require_once('./templates/head.html');
-  //Si la superglobal $_GET contient 1 élément ou plus
-  if(count($_GET) !== 0) {
-    $documentState = file_exists('./pages/' . $_GET["myPage"] . '.html');
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+	<meta charset="utf-8">
+	<title>Czaja Kevin, junior web developer</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/css?family=Droid+Sans:400,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./templates/css/style.css">
+</head>
+<body>
+	<main>
+		<div class="mt-3 container jumbotron">
+			<div class="row">
+				<div class="col-6">
+									<img class="rounded img-fluid" src="./templates/images/head.jpg">
+				</div>
+				<div class="col-6">
+									<h1 class="display-3">Quiz 2 Furious</h1>
+									<hr>
+									<p class="lead">The ultimate Fast & Furious quiz</p>
+				</div>
+			</div>
+		</div>
+		<section class="container">
+			<form action="./script.php">
 
+				<div class="container score rounded">
+					<?php
+					if(isset($_SESSION["score"])) {
+						echo $_SESSION["score"] . "/20.<br>";
+					}
+					if(isset($_SESSION["comment"])) {
+						echo $_SESSION["comment"];
+					}
+					?>
+				</div>
 
-    if ($documentState) {
-      //Si le document existe
-      require_once('./pages/' . $_GET["myPage"] . '.html');
-    } else {
-      //Si le document n'existe pas
-      require_once('./pages/404.html');
-    }
-  } else //Si la superglobal $_GET contient 0 élément
-  {
-    require_once('./pages/404.html');
-    ;
-  }
-  require_once('./templates/foot.html');
+				<h4><span class="badge badge-dark">Question 1</span> En quelle année est sorti le premier Fast and Furious ?</h4>
+				<div class="d-flex justify-content-between form-group mt-3 mb-5">
+					<label class="form-check-label" for="q1r1">
+						<input class="form-check-input" type="radio" id="q1r1" name="q1" value="1999" checked>
+						1999
+					</label>
+					<label class="form-check-label" for="q1r2">
+						<input class="form-check-input" type="radio" id="q1r2" name="q1" value="2001">
+						2001
+					</label>
+					<label class="form-check-label" for="q1r3">
+						<input class="form-check-input" type="radio" id="q1r3" name="q1" value="2004">
+						2003
+					</label>
+					<label class="form-check-label" for="q1r4">
+						<input class="form-check-input" type="radio" id="q1r4" name="q1" value="2007">
+						2005
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r1"])) {
+						echo $_SESSION["r1"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 2</span> Combien de films compte la franchise ?</h4>
+				<div class="form-group mt-3 mb-5">
+					<input class="form-control" type="number" name="q2" placeholder="reponse" required>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r2"])) {
+						echo $_SESSION["r2"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 3</span> Quelle est la boisson préférée de Toretto ?</h4>
+				<div class="d-flex justify-content-between form-check mt-3 mb-5">
+					<label class="form-check-label" for="q3r1">
+						<input class="form-check-input" type="radio" id="q3r1" name="q3" value="Cacolac" checked>
+						Cacolac
+					</label>
+					<label class="form-check-label" for="q3r2">
+						<input class="form-check-input" type="radio" id="q3r2" name="q3" value="8-6">
+						8-6
+					</label>
+					<label class="form-check-label" for="q3r3">
+						<input class="form-check-input" type="radio" id="q3r3" name="q3" value="Corona">
+						Corona
+					</label>
+					<label class="form-check-label" for="q3r4">
+						<input class="form-check-input" type="radio" id="q3r4" name="q3" value="Bud Light">
+						Bud Light
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r3"])) {
+						echo $_SESSION["r3"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 4</span> Quelle voiture conduit Brian au début de 2 Fast 2 Furious ?</h4>
+				<div class="d-flex justify-content-between form-check mt-3 mb-5">
+					<label class="mr-3 form-check-label" for="q4r1">
+						<input class="form-check-input" type="radio" id="q4r1" name="q4" value="Nissan Skyline" checked>
+						Nissan Skyline
+					</label>
+					<label class="form-check-label" for="q4r2">
+						<input class="form-check-input" type="radio" id="q4r2" name="q4" value="Renault Kangoo">
+						Renault Kangoo
+					</label>
+					<label class="form-check-label" for="q4r3">
+						<input class="form-check-input" type="radio" id="q4r3" name="q4" value="Honda s2000">
+						Honda s2000
+					</label>
+					<label class="form-check-label" for="q4r4">
+						<input class="form-check-input" type="radio" id="q4r4" name="q4" value="Renault Fuego">
+						Renault Fuego
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r4"])) {
+						echo $_SESSION["r4"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 5</span> Quel(s) rappeur(s) est/sont présent(s) dans la franchise ?</h4>
+				<div class="d-flex justify-content-between form-check mt-3 mb-5">
+					<label class="form-check-label" for="q5r1">
+						<input class="form-check-input" type="checkbox" id="q5r1" name="q5r1" value="Matt Houston">
+						Matt Houston
+					</label>
+					<label class="form-check-label" for="q5r2">
+						<input class="form-check-input" type="checkbox" id="q5r2" name="q5r2" value="Ja Rule">
+						Ja Rule
+					</label>
+					<label class="form-check-label" for="q5r3">
+						<input class="form-check-input" type="checkbox" id="q5r3" name="q5r3" value="Ludacris">
+						Ludacris
+					</label>
+					<label class="form-check-label" for="q5r4">
+						<input class="form-check-input" type="checkbox" id="q5r4" name="q5r4" value="50 Cent">
+						50 Cent
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r5"])) {
+						echo $_SESSION["r5"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 6</span> Où se déroule Fast & Furious 5 ?</h4>
+				<div class="d-flex justify-content-between form-group mt-3 mb-5">
+					<label class="form-check-label" for="q6r1">
+						<input class="form-check-input" type="radio" id="q6r1" name="q6" value="Los Angeles" checked>
+						Los Angeles
+					</label>
+					<label class="form-check-label" for="q6r2">
+						<input class="form-check-input" type="radio" id="q6r2" name="q6" value="Auchan">
+						Sur le parking d'Auchan à Trappes
+					</label>
+					<label class="form-check-label" for="q6r3">
+						<input class="form-check-input" type="radio" id="q6r3" name="q6" value="Mexico">
+						Mexico
+					</label>
+					<label class="form-check-label" for="q6r4">
+						<input class="form-check-input" type="radio" id="q6r4" name="q6" value="Bresil">
+						Brésil
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r6"])) {
+						echo $_SESSION["r6"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 7</span> Quel véhicule complètement probable poursuit le groupe à la fin de Fast & Furious 8 ?</h4>
+				<div class="d-flex justify-content-between form-group mt-3 mb-5">
+					<label class="form-check-label" for="q7r1">
+						<input class="form-check-input" type="radio" id="q7r1" name="q7" value="Sous-marin nucléaire" checked>
+						Sous-marin nucléaire
+					</label>
+					<label class="form-check-label" for="q7r2">
+						<input class="form-check-input" type="radio" id="q7r2" name="q7" value="Tank">
+						Tank
+					</label>
+					<label class="form-check-label" for="q7r3">
+						<input class="form-check-input" type="radio" id="q7r3" name="q7" value="Pope mobile">
+						Pope mobile
+					</label>
+					<label class="form-check-label" for="q7r4">
+						<input class="form-check-input" type="radio" id="q7r4" name="q7" value="Avion cargo">
+						Avion cargo
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r7"])) {
+						echo $_SESSION["r7"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 8</span> A l'heure actuelle, qui est encore vivant dans la franchise ?</h4>
+				<div class="d-flex justify-content-between form-check mt-3 mb-5">
+					<label class="form-check-label" for="q8r1">
+						<input class="form-check-input" type="checkbox" id="q8r1" name="q8r1" value="Brian">
+						Brian O'Connor
+					</label>
+					<label class="form-check-label" for="q8r2">
+						<input class="form-check-input" type="checkbox" id="q8r2" name="q8r2" value="Han">
+						Han
+					</label>
+					<label class="form-check-label" for="q8r3">
+						<input class="form-check-input" type="checkbox" id="q8r3" name="q8r3" value="Mia">
+						Mia
+					</label>
+					<label class="form-check-label" for="q8r4">
+						<input class="form-check-input" type="checkbox" id="q8r4" name="q8r4" value="Roman">
+						Roman
+					</label>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r8"])) {
+						echo $_SESSION["r8"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 9</span> Combien de nouveaux films ont été annoncés pour la franchise ?</h4>
+				<div class="form-group mt-3 mb-5">
+					<input class="form-control" type="number" name="q9" placeholder="reponse" required>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r9"])) {
+						echo $_SESSION["r9"];
+					}
+					?>
+				</div>
+
+				<h4><span class="badge badge-dark">Question 10</span> Quel est le pire Fast and Furious ?</h4>
+				<div class="form-check mt-3 mb-5">
+					<select class="form-control custom-select" name="q10">
+						<option value="ff1">The Fast and the Furious</option>
+						<option value="ff2">2 Fast 2 Furious</option>
+						<option value="ff3">Fast and Furious: Tokyo Drift</option>
+						<option value="ff4">Fast & Furious</option>
+						<option value="ff5">Fast Five</option>
+						<option value="ff6">Fast & Furious 6</option>
+						<option value="ff7">Furious 7</option>
+						<option value="ff8">The Fate of the Furious</option>
+					</select>
+				</div>
+				<div class="container reponse rounded mb-3">
+					<?php
+					if(isset($_SESSION["r10"])) {
+						echo $_SESSION["r10"];
+					}
+					?>
+				</div>
+
+				<input class="mb-3 btn btn-danger"type="submit" value="Valider les réponses">
+
+			</form>
+		</section>
+	</main>
+	<?php
+		unset($_SESSION["r1"]);
+		unset($_SESSION["r2"]);
+		unset($_SESSION["r3"]);
+		unset($_SESSION["r4"]);
+		unset($_SESSION["r5"]);
+		unset($_SESSION["r6"]);
+		unset($_SESSION["r7"]);
+		unset($_SESSION["r8"]);
+		unset($_SESSION["r9"]);
+		unset($_SESSION["r10"]);
+		unset($_SESSION["score"]);
+		unset($_SESSION["comment"]);
+	 ?>
+</body>
+</html>
--- script.php
+++ script.php
@@ -1,70 +1,122 @@
 <?php
 
-/*
+
 // Start the session
 session_start();
-  $_SESSION["r1"] = "2001";
-*/
+
 
 $score = 0;
 
 //Teste la Q1
 if ($_GET["q1"] == "2001") {
-  echo "Question 1: Correct !";
-  $score += 2;
+  //echo "Question 1: Correct !<br>";
+  $_SESSION["r1"] = "Question 1: Correct !";
+  //$score += 2;
 } else {
-  echo "Question 1: Faux, la bonne réponse était 2001.";
+  //echo "Question 1: Faux, la bonne réponse était 2001.<br>";
+  $_SESSION["r1"] = "Question 1: Faux, la bonne réponse était 2001.";
 }
 
 //Teste la Q2
 if ($_GET["q2"] == "8") {
-  echo "Question 2: Correct !";
+  //echo "Question 2: Correct !<br>";
+  $_SESSION["r2"] = "Question 2: Correct !";
   $score += 2;
 } else {
-  echo "Question 2: Faux, la bonne réponse était 8.";
+  //echo "Question 2: Faux, la bonne réponse était 8.<br>";
+  $_SESSION["r2"] = "Question 2: Faux, la bonne réponse était 8.";
 }
 
 //Teste la Q3
 if ($_GET["q3"] == "Corona") {
-  echo "Question 3: Correct !";
+  //echo "Question 3: Correct !<br>";
+  $_SESSION["r3"] = "Question 3: Correct !";
   $score += 2;
 } else {
-  echo "Question 3: Faux, la bonne réponse était Corona.";
+  //echo "Question 3: Faux, la bonne réponse était Corona.<br>";
+  $_SESSION["r3"] = "Question 3: Faux, la bonne réponse était Corona.";
 }
 
 //Teste la Q4
 if ($_GET["q4"] == "Nissan Skyline") {
-  echo "Question 4: Correct !";
+  //echo "Question 4: Correct !<br>";
+  $_SESSION["r4"] = "Question 4: Correct !";
   $score += 2;
 } else {
-  echo "Question 4: Faux, la bonne réponse était Nissan Skyline.";
+  $_SESSION["r4"] = "Question 4: Faux, la bonne réponse était Nissan Skyline.<br>";
 }
 
 //Teste la Q5
 if (isset($_GET["q5r2"]) && isset($_GET["q5r3"])) {
-  echo "Question 5: Correct !";
+  //echo "Question 5: Correct !<br>";
+  $_SESSION["r5"] = "Question 5: Correct !";
   $score += 2;
 } else {
-  echo "Question 5: Faux, la bonne réponse était Ja Rule et Ludacris.";
+  $_SESSION["r5"] = "Question 5: Faux, la bonne réponse était Ja Rule et Ludacris.<br>";
+}
+
+//Teste la Q6
+if ($_GET["q6"] == "Bresil") {
+  //echo "Question 6: Correct !<br>";
+  $_SESSION["r6"] = "Question 6: Correct !";
+  $score += 2;
+} else {
+  $_SESSION["r6"] = "Question 6: Faux, THIS IS BRAZILLLL !§§!<br>";
+}
+
+//Teste la Q7
+if ($_GET["q7"] == "Sous-marin nucléaire") {
+  //echo "Question 7: Correct !<br>";
+  $_SESSION["r7"] = "Question 7: Correct !";
+  $score += 2;
+} else {
+  $_SESSION["r8"] = "Question 7: Faux, il s'agit d'un sous-marin nucléaire, narmol.<br>";
+}
+
+//Teste la Q8
+if (isset($_GET["q8r1"]) && isset($_GET["q5r3"]) && isset($_GET["q8r4"])) {
+  //echo "Question 8: Correct !<br>";
+  $_SESSION["r8"] = "Question 8: Correct !";
+  $score += 2;
+} else {
+  $_SESSION["r8"] = "Question 8: Faux, la bonne réponse était Brian, Mia et Roman.<br>";
+}
+
+//Teste la Q9
+if ($_GET["q9"] == "2") {
+  //echo "Question 2: Correct !<br>";
+  $_SESSION["r9"] = "Question 9: Correct !";
+  $score += 2;
+} else {
+  $_SESSION["r9"] = "Question 2: Faux, la bonne réponse était 8.<br>";
 }
 
 //Teste la Q10
 if ($_GET["q10"] == "ff3") {
-  echo "Question 10: Ok, on est sur la même longueur d'onde.";
+  //echo "Question 10: Ok, on est sur la même longueur d'onde.<br>";
+  $_SESSION["r10"] = "Question 10: Correct !";
   $score += 2;
 } else {
-  echo "Tu n'as pas répondu Fast and Furious: Tokyo Drift, tu viens donc de perdre 2 points.";
+  $_SESSION["r10"] = "Tu n'as pas répondu Fast and Furious: Tokyo Drift, tu viens donc de perdre 2 points.<br>";
   $score -= 2;
 }
 
 //Score
-echo "Votre score est de " . $score . "/20.";
+$_SESSION["score"] = $score;
+
+//echo "Votre score est de " . $score . "/20.<br>";
 
 //Avis sur le score
 if ($score >= 16) {
-  echo "Drift King !";
-} else if ($score < 0) {
-  echo "Tu n'y connais rien et en plus, Tokyo Drift n'est pas le pire Fast & Furious pour toi...";
-} else {
-  echo "Vous êtes du genre à rouler en Multipla non?";
+  $_SESSION["comment"] = "Drift King !";
 }
+else if ($score < 16 && $score >= 10) {
+  $_SESSION["comment"] = "Pas fou mais tout n'est pas perdu.";
+}
+else if ($score < 0) {
+  $_SESSION["comment"] = "Tu n'y connais rien mais en plus, tu penses que Fast and Furious: Tokyo Drift n'est pas le pire de la franchise...<br>";
+} else {
+  $_SESSION["comment"] = "Naze, tu es du genre à rouler en Multipla non?";
+}
+
+header('Location: ./index.php');
